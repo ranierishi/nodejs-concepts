7b1fb1d140299e89f9e192f5227136a44dfca068
Ranieri <ranieri.shi@gmail.com>
2020-10-27 15:39:48 -0300

    routes get, post and put

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { uuid, isUuid } = require('uuidv4')
 const cors = require("cors");
 
 // const { v4: uuid, validate: isUuid } = require('uuid');
@@ -11,15 +12,56 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  return response.json(repositories)
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    console.log('1')
+    const { title, url, techs} = request.body
+    console.log('2')
+
+    const repository = {
+      id: uuid(),
+      title,
+      url,
+      techs,
+      likes: 0
+    }
+    
+    if(!isUuid(repository.id)){
+      return response.status(400).json({error: 'id inválido'})
+    }
+    repositories.push(repository)
+
+    return response.json(repository)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params
+
+  const result = repositories.findIndex(repository => repository.id === id)
+
+  if(result < 0){
+    return response.status(400).json({error: 'id inválido'})
+  }
+
+  const {title, url, techs} = request.body
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[result].likes
+  }
+
+  repositories[result] = repository
+
+  return response.json(repository)
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
